db2d11f0eab35384f58dacbfb588e5ee3440737e
famezeeshan <zeeshan6143@gmail.com>
2025-10-28 11:58:50 +0530

    fixing Data analytics issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./components/ThemeContext";
-import { GoogleAnalytics } from "@next/third-parties/google"; // Add this
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
